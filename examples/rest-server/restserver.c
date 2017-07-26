d499358fedccc83b73578ec367a6ad2e52707398
Giedrius Medzevicius <giedrius@8devices.com>
2017-07-26 14:23:46 +0000

    Add initial implementation for rest server
    Currently it's only a light version of lwm2m server, without any rest support

---
+++ examples/rest-server/restserver.c
@@ -0,0 +1,177 @@
+
+#include <liblwm2m.h>
+
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+
+#include "connection.h"
+
+
+const char * binding_to_string(lwm2m_binding_t bind)
+{
+    switch (bind)
+    {
+    case BINDING_U:
+        return "U";
+    case BINDING_UQ:
+        return "UQ";
+    case BINDING_S:
+        return "S";
+    case BINDING_SQ:
+        return "SQ";
+    case BINDING_US:
+        return "US";
+    case BINDING_UQS:
+        return "UQS";
+    default:
+        return "Unknown";
+    }
+}
+
+void client_monitor_cb(uint16_t clientID, lwm2m_uri_t * uriP, int status, lwm2m_media_type_t format, uint8_t * data, int dataLength, void * userData)
+{
+    lwm2m_context_t *lwm2m = (lwm2m_context_t *)userData;
+    lwm2m_client_t *client;
+    lwm2m_client_object_t *obj;
+    lwm2m_list_t *ins;
+
+    switch (status)
+    {
+    case COAP_201_CREATED:
+    case COAP_204_CHANGED:
+
+        if (status == COAP_201_CREATED)
+        {
+            fprintf(stdout, "[MONITOR] Client %d registered.\n", clientID);
+        } 
+        else 
+        {
+            fprintf(stdout, "[MONITOR] Client %d updated.\n", clientID);
+        }
+        
+        client = (lwm2m_client_t *)lwm2m_list_find((lwm2m_list_t *)lwm2m->clientList, clientID);
+        fprintf(stdout, "\tname: '%s'\n", client->name);
+        fprintf(stdout, "\tbind: '%s'\n", binding_to_string(client->binding));
+        fprintf(stdout, "\tlifetime: %d\n", client->lifetime);
+        fprintf(stdout, "\tobjects: ");
+        for (obj = client->objectList; obj != NULL; obj = obj->next)
+        {
+            if (obj->instanceList == NULL)
+            {
+                fprintf(stdout, "/%d, ", obj->id);
+            }
+            else
+            {
+                for (ins = obj->instanceList; ins != NULL; ins = ins->next)
+                {
+                    fprintf(stdout, "/%d/%d, ", obj->id, ins->id);
+                }
+            }
+        }
+        fprintf(stdout, "\n");
+        break;
+
+    case COAP_202_DELETED:
+        fprintf(stdout, "[MONITOR] Client %d deregistered.\n", clientID);
+        break;
+    default:
+        fprintf(stdout, "[MONITOR] Client %d status update %d.\n", clientID, status);
+        break;
+    }
+}
+
+int socket_receive(lwm2m_context_t *lwm2m, int sock)
+{
+    int nbytes;
+    uint8_t buf[1500];
+    struct sockaddr_storage addr;
+    socklen_t addrLen;
+    connection_t *con;
+    static connection_t *connectionList = NULL;
+
+    memset(buf, 0, sizeof(buf));
+
+    nbytes = recvfrom(sock, buf, sizeof(buf), 0, (struct sockaddr *)&addr, &addrLen);
+
+    if (nbytes < 0)
+    {
+        fprintf(stderr, "recvfrom() error: %d\n", nbytes);
+        return -1;
+    }
+
+    con = connection_find(connectionList, &addr, addrLen);
+    if (con == NULL)
+    {
+        con = connection_new_incoming(connectionList, sock, (struct sockaddr *)&addr, addrLen);
+        if (con)
+        {
+            connectionList = con;
+        }
+    }
+
+    if (con)
+    {
+        lwm2m_handle_packet(lwm2m, buf, nbytes, con);
+    }
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    int sock;
+    struct sockaddr_in saddr;
+    fd_set readfds;
+    struct timeval tv;
+    int res;
+    lwm2m_context_t *lwm2m;
+
+
+    /* Socket section */
+    sock = create_socket("5432", AF_INET6);
+    if (sock < 0)
+    {
+        fprintf(stderr, "Failed to create socket!\n");
+        return -1;
+    }
+
+    /* Server section */
+    lwm2m = lwm2m_init(NULL);
+    if (lwm2m == NULL)
+    {
+        fprintf(stderr, "Failed to create LwM2M server!\n");
+        return -1;
+    }
+    
+    lwm2m_set_monitoring_callback(lwm2m, client_monitor_cb, lwm2m);
+
+    while (1)
+    {
+        FD_ZERO(&readfds);
+        FD_SET(sock, &readfds);
+
+        tv.tv_sec = 5;
+        tv.tv_usec = 0;
+
+        res = lwm2m_step(lwm2m, &tv.tv_sec);
+        if (res)
+        {
+            fprintf(stderr, "lwm2m_step() error: %d\n", res);
+        }
+
+        res = select(FD_SETSIZE, &readfds, NULL, NULL, &tv);
+        if (res < 0)
+        {
+            fprintf(stderr, "select() error: %d\n", res);
+        }
+
+        if (FD_ISSET(sock, &readfds))
+        {
+            socket_receive(lwm2m, sock);
+        }
+
+    }
+
+}
