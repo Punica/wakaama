3133484732180ec6f47a345db7ef84627810ce8d
Giedrius <giedrius.medzevicius@gmail.com>
2018-04-04 10:41:13 +0300

    Use ulfius v2.3.2 (#51)

--- .travis/install_ulfius.sh
+++ .travis/install_ulfius.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git clone https://github.com/babelouest/ulfius.git
+git clone -b v2.3.2 --single-branch https://github.com/babelouest/ulfius.git
 cd ulfius/
 git submodule update --init
 
