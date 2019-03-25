bcf3038d25ee4675bf4f1acc8b8e708a84af1919
Tautvydas Belgeras <tautvydas.b@8devices.com>
2019-03-25 14:33:59 +0200

    Add '-DCUSTOM_CONNECTION_HANDLING' option

--- examples/shared/shared.cmake
+++ examples/shared/shared.cmake
@@ -7,25 +7,27 @@ set(SHARED_SOURCES
     ${SHARED_SOURCES_DIR}/platform.c
 	${SHARED_SOURCES_DIR}/memtrace.c)
 
-if(DTLS)
-    include(${CMAKE_CURRENT_LIST_DIR}/tinydtls.cmake)
-
-    set(SHARED_SOURCES
-	    ${SHARED_SOURCES}
-		${TINYDTLS_SOURCES}
-		${SHARED_SOURCES_DIR}/dtlsconnection.c)
+if(NOT CUSTOM_CONNECTION_HANDLING)
+    if(DTLS)
+        include(${CMAKE_CURRENT_LIST_DIR}/tinydtls.cmake)
     
-	set(SHARED_INCLUDE_DIRS
-		${SHARED_SOURCES_DIR} 
-		${TINYDTLS_SOURCES_DIR})
-    
-	set(SHARED_DEFINITIONS -DWITH_TINYDTLS)
-else()
-    set(SHARED_SOURCES
-		${SHARED_SOURCES} 
-		${SHARED_SOURCES_DIR}/connection.c)
+        set(SHARED_SOURCES
+            ${SHARED_SOURCES}
+            ${TINYDTLS_SOURCES}
+            ${SHARED_SOURCES_DIR}/dtlsconnection.c)
 
-    set(SHARED_INCLUDE_DIRS ${SHARED_SOURCES_DIR})
+        set(SHARED_INCLUDE_DIRS
+            ${SHARED_SOURCES_DIR}
+            ${TINYDTLS_SOURCES_DIR})
+
+        set(SHARED_DEFINITIONS -DWITH_TINYDTLS)
+    else()
+        set(SHARED_SOURCES
+            ${SHARED_SOURCES}
+            ${SHARED_SOURCES_DIR}/connection.c)
+
+        set(SHARED_INCLUDE_DIRS ${SHARED_SOURCES_DIR})
+    endif()
 endif()
 
 
